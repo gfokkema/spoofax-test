fc71c71e7e23a0728d2c38bf910650eee2230469
Gerlof Fokkema <gerlof.fokkema@gmail.com>
2016-04-29 23:58:20 +0200

    lambda's and java 1.8

--- src/main/java/test/SpoofaxTest.java
+++ src/main/java/test/SpoofaxTest.java
@@ -1,7 +1,6 @@
 package test;
 
 import java.net.URL;
-import java.util.function.Consumer;
 
 import org.apache.commons.vfs2.FileObject;
 import org.metaborg.core.MetaborgException;
@@ -17,46 +16,49 @@ import org.metaborg.core.processing.NullProgressReporter;
 import org.metaborg.core.project.IProject;
 import org.metaborg.core.project.SimpleProjectService;
 import org.metaborg.spoofax.core.Spoofax;
-import org.metaborg.spoofax.core.unit.ISpoofaxTransformUnit;
 
 public class SpoofaxTest {
 //	final static String langPath = "org.metaborg.meta.lang.nabl-2.0.0-beta1.spoofax-language";
 //	final static String sourcePath = "test.nabl";
 	final static String langPath = "paplj.full";
 	final static String sourcePath = "fib.pj";
-	
-	public static void main(String[] args) throws InterruptedException {
+
+	public static void main(String[] args) {
 		try(final Spoofax spoofax = new Spoofax()) {
-			// Load zip file given by path
-			URL langUrl = SpoofaxTest.class.getClassLoader().getResource(langPath);
-			FileObject langLocation = spoofax.resourceService.resolve("zip:" + langUrl + "!/");
-
-			// Discover languages inside zip file
-			Iterable<ILanguageDiscoveryRequest> requests = spoofax.languageDiscoveryService.request(langLocation);
-			Iterable<ILanguageComponent> components = spoofax.languageDiscoveryService.discover(requests);
-
-			// Load the languages
-			ILanguageImpl lang = LanguageUtils.active(LanguageUtils.toImpls(components));
-			if(lang == null) throw new MetaborgException("No language implementation was found");
-
-			// Initalize project
-			IProject project = spoofax.injector.getInstance(SimpleProjectService.class)
-									.create(spoofax.resourceService.resolve("~/project"));
-
-			// Context is managed by build input
-			BuildInput build = new BuildInputBuilder(project)
-										.addSource(spoofax.resourceService.resolve("res:" + sourcePath))
-										.addTransformGoal(new EndNamedGoal("Run"))
-										.build(spoofax.dependencyService, spoofax.languagePathService);
-			spoofax.builder.build(build, new NullProgressReporter(), new CancellationToken()).transformResults()
-				.forEach(new Consumer<ISpoofaxTransformUnit<?>>() {
-					@Override
-					public void accept(ISpoofaxTransformUnit<?> t) {
-						System.out.println("Output: " + t.ast());
-					}
-			});
-		} catch(MetaborgException e) {
+			new SpoofaxTest().run(spoofax);
+		} catch(MetaborgException | InterruptedException e) {
 			e.printStackTrace();
 		}
+
+	}
+	
+	public void run(Spoofax spoofax) throws InterruptedException, MetaborgException {
+		// Load zip file given by path
+		URL langUrl = SpoofaxTest.class.getClassLoader().getResource(langPath);
+		FileObject langLocation = spoofax.resourceService.resolve("zip:" + langUrl + "!/");
+
+		// Discover languages inside zip file
+		Iterable<ILanguageDiscoveryRequest> requests = spoofax.languageDiscoveryService.request(langLocation);
+		Iterable<ILanguageComponent> components = spoofax.languageDiscoveryService.discover(requests);
+
+		// Load the languages
+		ILanguageImpl lang = LanguageUtils.active(LanguageUtils.toImpls(components));
+		if(lang == null) throw new MetaborgException("No language implementation was found");
+
+		// Initalize project
+		IProject project = spoofax.injector
+				.getInstance(SimpleProjectService.class)
+				.create(spoofax.resourceService.resolve("/tmp/project"));
+
+		// Context is managed by build input
+		BuildInput build = new BuildInputBuilder(project)
+				.addSource(spoofax.resourceService.resolve("res:" + sourcePath))
+				.addTransformGoal(new EndNamedGoal("Run"))
+				.build(spoofax.dependencyService, spoofax.languagePathService);
+		
+		spoofax.builder
+				.build(build, new NullProgressReporter(), new CancellationToken())
+				.transformResults()
+				.forEach(t -> System.out.println("Output: " + t.ast()));
 	}
 }
